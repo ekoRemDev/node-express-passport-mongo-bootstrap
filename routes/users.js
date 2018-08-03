68d061ae8729690b8de06fee6dc678beea8b7ee6
ekoremdev@gmail.com <ekoremdev@gmail.com>
2018-08-03 17:17:08 +0400

    User Register Form and controls are Created

--- routes/users.js
+++ routes/users.js
@@ -4,13 +4,51 @@ const express = require('express');
 const router = express.Router();
 
 // Register
-router.get('/register',(req,res)=>{
+router.get('/register', (req, res) => {
     res.render('register');
 });
 
 // Login
-router.get('/login',(req,res)=>{
+router.get('/login', (req, res) => {
     res.render('login');
+});
+
+router.post('/register', (req, res) => {
+    // const newUser = {
+    //     name : req.body.name,
+    //     email : req.body.email,
+    //     username : req.body.username,
+    //     password : req.body.password,
+    //     password2 : req.body.password2,
+    // };
+    // res.send(newUser)
+
+    // We are getting form input values
+    let name = req.body.name;
+    let email = req.body.email;
+    let username = req.body.username;
+    let password = req.body.password;
+    let password2 = req.body.password2;
+
+    // we're gonna do validation
+    req.checkBody('name', 'Name is required').notEmpty();
+    req.checkBody('email', 'Email is required').notEmpty();
+    req.checkBody('email', 'Email is not valid').isEmail();
+    req.checkBody('username', 'Username is required').notEmpty();
+    req.checkBody('password', 'Password is required').notEmpty();
+    req.checkBody('password2', 'Name is required').equals(req.body.password);
+
+    var errors = req.validationErrors();
+
+    if (errors) {
+        res.render('register',{
+            errors:errors
+        })
+    } else {
+        console.log('PASSED');
+    }
+
+
 });
 
 module.exports = router;
